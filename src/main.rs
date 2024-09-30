5afac2ca13575ed7df7b2c1a8cf0be6897025552
Clément <86530475+clement-crn@users.noreply.github.com>
2024-09-30 21:18:39 +0200

    get city infos from data.gouv

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use dotenv::dotenv;
+use reqwest::Url;
+use serde_json::Value;
 use std::env;
 
-fn fetch_weather() {
+async fn fetch_weather() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let api_key = env::var("API_KEY").expect("API_KEY not found in .env file");
+    let api_key = env::var("API_KEY")?;
     println!("API Key: {}", api_key);
+
+    // Define an object coordinates that will hold the latitude and longitude of the city
+    let url = Url::parse("https://api-adresse.data.gouv.fr/search/?q=soissons")?;
+    let response = reqwest::get(url).await?;
+    let coordinates: Value = response.json().await?;
+    println!("Coordinates: {:?}", coordinates);
+
+    Ok(())
 }
 
-fn main() {
-    fetch_weather();
+#[tokio::main]
+async fn main() {
+    if let Err(e) = fetch_weather().await {
+        eprintln!("Error fetching weather: {}", e);
+    }
 }
