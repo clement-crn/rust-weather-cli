07c052860e8aeb8703ac58e864e83df27c272a21
Clément <86530475+clement-crn@users.noreply.github.com>
2024-09-30 21:05:14 +0200

    init api key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use dotenv::dotenv;
+use std::env;
+
+fn fetch_weather() {
+    dotenv().ok();
+    let api_key = env::var("API_KEY").expect("API_KEY not found in .env file");
+    println!("API Key: {}", api_key);
+}
+
 fn main() {
-    println!("Hello, world!");
+    fetch_weather();
 }
