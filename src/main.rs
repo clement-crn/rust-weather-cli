e7e7c366ac76c52126c33374f766bc5141e3d6fe
Clément <86530475+clement-crn@users.noreply.github.com>
2024-09-30 21:24:10 +0200

    ask to the user a city name

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,22 @@ use reqwest::Url;
 use serde_json::Value;
 use std::env;
 
-async fn fetch_weather() -> Result<(), Box<dyn std::error::Error>> {
+async fn ask_city() -> Result<String, Box<dyn std::error::Error>> {
+    println!("Enter the city name:");
+    let mut city = String::new();
+    std::io::stdin().read_line(&mut city)?;
+    Ok(city.trim().to_string())
+}
+
+async fn fetch_weather(city: &str) -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let api_key = env::var("API_KEY")?;
     println!("API Key: {}", api_key);
 
-    // Define an object coordinates that will hold the latitude and longitude of the city
-    let url = Url::parse("https://api-adresse.data.gouv.fr/search/?q=soissons")?;
+    let url = Url::parse(&format!(
+        "https://api-adresse.data.gouv.fr/search/?q={}",
+        city
+    ))?;
     let response = reqwest::get(url).await?;
     let coordinates: Value = response.json().await?;
     println!("Coordinates: {:?}", coordinates);
@@ -19,7 +28,12 @@ async fn fetch_weather() -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() {
-    if let Err(e) = fetch_weather().await {
-        eprintln!("Error fetching weather: {}", e);
+    match ask_city().await {
+        Ok(city_name) => {
+            if let Err(e) = fetch_weather(&city_name).await {
+                eprintln!("Error fetching weather: {}", e);
+            }
+        }
+        Err(e) => eprintln!("Error reading city name: {}", e),
     }
 }
