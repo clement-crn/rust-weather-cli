7e981f619012cbe03e92f33f4c5f2f9a903ff084
Clément <86530475+clement-crn@users.noreply.github.com>
2024-10-01 22:50:13 +0200

    api get weather

--- src/main.rs
+++ src/main.rs
@@ -10,30 +10,49 @@ async fn ask_city() -> Result<String, Box<dyn std::error::Error>> {
     Ok(city.trim().to_string())
 }
 
-async fn fetch_weather(city: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn fetch_coordinates(city: &str) -> Result<(f64, f64), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let api_key = env::var("API_KEY")?;
-    println!("API Key: {}", api_key);
-
     let url = Url::parse(&format!(
         "https://api-adresse.data.gouv.fr/search/?q={}",
         city
     ))?;
     let response = reqwest::get(url).await?;
     let coordinates: Value = response.json().await?;
-    println!("Coordinates: {:?}", coordinates);
+    let lat = coordinates["features"][0]["geometry"]["coordinates"][1]
+        .as_f64()
+        .ok_or("Latitude not found")?;
+    let lon = coordinates["features"][0]["geometry"]["coordinates"][0]
+        .as_f64()
+        .ok_or("Longitude not found")?;
+    println!("Coordinates: lat={}, lon={}", lat, lon);
+    Ok((lat, lon))
+}
 
+async fn fetch_weather(lat: f64, lon: f64) -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let api_key = env::var("API_KEY")?;
+    println!("API Key: {}", api_key);
+
+    let url = Url::parse(&format!(
+        "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    ))?;
+    let response = reqwest::get(url).await?;
+    let weather: Value = response.json().await?;
+    println!("Weather: {:?}", weather);
     Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     match ask_city().await {
-        Ok(city_name) => {
-            if let Err(e) = fetch_weather(&city_name).await {
-                eprintln!("Error fetching weather: {}", e);
+        Ok(city_name) => match fetch_coordinates(&city_name).await {
+            Ok((lat, lon)) => {
+                if let Err(e) = fetch_weather(lat, lon).await {
+                    eprintln!("Error fetching weather: {}", e);
+                }
             }
-        }
+            Err(e) => eprintln!("Error fetching coordinates: {}", e),
+        },
         Err(e) => eprintln!("Error reading city name: {}", e),
     }
 }
